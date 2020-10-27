88c238225e5c29ecaece0a0e898828c67fbad295
vyletat <vyleta.tom@gmail.com>
2020-10-27 09:58:42 +0100

    tables to HTML

--- Examples/Queries/js/queries.js
+++ Examples/Queries/js/queries.js
@@ -33,16 +33,62 @@ console.log(alasql.databases.myDatabase1);
 const myDatabase2 = new alasql.Database('myDatabase2');
 console.log(alasql.databases.myDatabase2);
 
+// Table creation
 myDatabase2.exec('CREATE TABLE cities (name string, population number)');
+// All columns of table cities
+console.table(alasql.databases.myDatabase2.tables.cities.columns);
 
+// Obecna funkce pro vypis
+var vypisVsechnaMesta = function() {
+    console.table(alasql.databases.myDatabase2.tables.cities.data);
+};
+const ceskaMesta = [
+    {name: 'Prague', population: 1324277},      // Kdyz se sem da string, tak to nepozna, pokud to nejde pres sql
+    {name: 'Brno', population: 381346},
+    {name: 'Ostrava', population: 287968},
+    {name: 'Plzen', population: 174842}
+];
+// Prirazeni objektu
+alasql.databases.myDatabase2.tables.cities.data = ceskaMesta;
+vypisVsechnaMesta();
+
+//Insert
 myDatabase2.exec("INSERT INTO cities VALUES ('Rome',2863223),('Paris',2249975),('Berlin',3517424),('Madrid',3041579)");
+vypisVsechnaMesta();
+
 myDatabase2.exec('SELECT * FROM cities', [],
     function (res) {
-        console.log(res);
+        console.table(res);
     }
 );
 
+// Vysledek query jako promenna
 const allCities = myDatabase2.exec("SELECT * FROM cities WHERE population < 3500000 ORDER BY population DESC");
 console.log(allCities);
 
+// Update
 myDatabase2.exec('UPDATE cities SET population = population * 1.5 WHERE name LIKE "A%"');
+
+console.log(alasql.databases.myDatabase2);
+
+
+alasql('CREATE TABLE cisla (one INT, two INT)');
+alasql('INSERT INTO cisla VALUES (1,2), (3,4), (5,6)');
+alasql('SELECT * INTO HTML("#sampleTable", {headers:true}) FROM cisla');
+
+myDatabase2.exec('SELECT * INTO HTML("#cities", {headers:true}) FROM cities');
+
+//Custom SQL functions
+alasql.fn.myFunction = function (population) {
+  return (population / 1000);
+};
+var result = alasql('SELECT myFunction(population) FROM cities');
+console.log(result);
+// Jdou i vlastni agregacni funkce
+
+var ins = alasql.databases.myDatabase2.compile('INSERT INTO cities VALUES (?,?)');
+ins('mesto1',10);
+ins('mesto2',20);
+vypisVsechnaMesta();
+
+// Acesss object propertires - obj->property->subproperty
