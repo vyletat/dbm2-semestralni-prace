5dfd358cc8ecfc7553203edd6cb868f86c09ba51
vyletat <vyleta.tom@gmail.com>
2020-11-23 10:43:08 +0100

    Finishing

--- Examples/Queries/js/starcraft.js
+++ Examples/Queries/js/starcraft.js
@@ -3,7 +3,6 @@ $.getJSON('../data/ladders-eu-230882.json', function(data) {
     console.log(data);
 });
 
-
 /* Inicializace databaze*/
 const starcraftDatabase = new alasql.Database('starcraftDatabase');
 
@@ -59,20 +58,33 @@ console.log(test);*/
  * @param tier
  * @param divisionIndex
  */
-function sumPlayersFromDivision(tier, divisionIndex) {
+function sumPlayersFromDivision(tier) {
     //division = alasql('SELECT division FROM ? WHERE id=?', [tier.tier, divisionID]);
     console.info("Kolik hráčů je v jednotlivých divizích/úrovních? (dle JSONů typu A - \"member_count\")");
-    console.log(alasql('SELECT SUM(member_count) AS member_count FROM ?', [tier.tier[divisionIndex].division]));
+    let counter = 0;
+    for (let i = 0; i < tier.tier.length; i++) {
+        memberCount = alasql('SELECT SUM(member_count) AS member_count FROM ?', [tier.tier[i].division]);
+        console.log('Tier ' + tier.key.league_id + ' - divize s id ' + tier.tier[i].id + ': ' + memberCount[0].member_count);
+        counter  += memberCount[0].member_count;
+    }
+    console.log('Celkovy pocet hracu v tieru ' + tier.key.league_id + ' je ' + counter);
 }
-sumPlayersFromDivision(tier5, 0);
+sumPlayersFromDivision(tier5);
+sumPlayersFromDivision(tier6);
 
 
 /* Jsou konzistentní počty hráčů mezi JSON typ A a B? ("member_count" v A vs "team" v B) */
-console.info("Jsou konzistentní počty hráčů mezi JSON typ A a B? (\"member_count\" v A vs \"team\" v B)");
-console.log(alasql('SELECT member_count FROM ? WHERE ladder_id=230898', [tier5.tier[0].division]));
-// stejny ladder
-console.log(alasql('SELECT COUNT(*) AS member_count FROM ?', [ladder230898_1031.team]));
-console.log(alasql('SELECT team->length FROM ?', [ladder230898_1031.team]));
+function ladderMemberCountConsist(tier, ladderId) {
+    console.info("Jsou konzistentní počty hráčů mezi JSON typ A a B? (\"member_count\" v A vs \"team\" v B)");
+    /*console.log(alasql('SELECT member_count FROM ? WHERE ladder_id=230898', [tier5.tier[0].division]));
+    console.log(alasql('SELECT COUNT(*) AS member_count FROM ?', [ladder230898_1031.team]));*/
+    count = alasql('SELECT member_count FROM ? WHERE ladder_id= ?;SELECT COUNT(*) AS member_count FROM ?', [tier.tier[0].division, ladderId, ladder230898_1031.team])
+    console.log(count);
+    console.log('Jsou pocty hracu konzisteni? - ' + (count[0][0].member_count === count[1][0].member_count ? 'ano.' : 'ne.'));
+}
+ladderMemberCountConsist(tier5, 230898);
+
+// TODO JOIN - jde jen s tabulkami
 
 
 /**
@@ -93,12 +105,11 @@ orderPlayersFromLadder(ladder230898_1031);
  * Jací hráči vybočují z hranic divizí? (tj. mají ELO jiné než z rozsahu "min_rating" - "max_rating" v A)
  *
  * @param tier
- * @param id
  */
-function playersElo(tier, id) {
+function playersElo(tier, divisionId) {
     console.info("Jací hráči vybočují z hranic divizí? (tj. mají ELO jiné než z rozsahu \"min_rating\" - \"max_rating\" v A)");
-    minMax = alasql('SELECT max_rating, min_rating FROM ?', [tier.tier]);
-    alasql('SELECT division->(0)->ladder_id FROM (SELECT division FROM ?)', [tier.tier]); // vybere prvni ladder z kazde divize PROBLEM popsat!
+    /*minMax = alasql('SELECT max_rating, min_rating FROM ?', [tier.tier]);
+    alasql('SELECT division->(0)->ladder_id FROM (SELECT division FROM ?)', [tier.tier]);
     const ladderArray = [];
     alasql('SELECT division FROM ?', [tier.tier]).forEach(division => {
         division.division.forEach(ladder => {
@@ -106,10 +117,18 @@ function playersElo(tier, id) {
         });
         console.log(ladderArray);
     });
-    console.log(alasql('SELECT member->(0)->legacy_link->name FROM ? WHERE rating NOT BETWEEN 4360 AND 4591', [ladder230882_1101.team]));
+    console.log(alasql('SELECT member->(0)->legacy_link->name FROM ? WHERE rating NOT BETWEEN 4360 AND 4591', [ladder230882_1101.team]));*/
 
+    // Najdu min a max divize, najdu vsechny ladder id
+    // vzberu vsechny hrace ladderu s id a ELO
+    // vzberu hrace, kteri nemaji ELO mezi min a max
+    division = alasql('SELECT * FROM ? WHERE id= ?', [tier.tier, divisionId]);
+    divisionLadders = alasql("SELECT ladder_id AS lad_id FROM ?", [division[0].division]);
+    // nebo - vybereme divizi a JOINem spojime s ladder ID (SELECT ladder.team FROM ladder INNER JOIN division ON division.ladder=ladder.id)
+    // hypoteticky - pro kazdy ladder vybereme hrace, kteri nejsou v rozsahu min/max dane divize
+    console.log(alasql('SELECT member->(0)->legacy_link->name AS name, rating FROM ? WHERE rating NOT BETWEEN ? AND ?', [ladder230882_1101.team, division[0].min_rating, division[0].max_rating]));
 }
-playersElo(tier5);
+playersElo(tier5, 2);
 
 
 /**
@@ -125,6 +144,7 @@ function lastPlayedFrom(ladderJson, timeFromInMilis) {
     console.log(alasql.exec('SELECT COUNT(*) FROM ? WHERE last_played_time_stamp > ?', [ladderJson.team, timeFromInMilis]));
 }
 lastPlayedFrom(ladder230898_1031, 1604079724);
+lastPlayedFrom(ladder230898_1031, 1602957543);
 //length zkusit
 
 
@@ -169,5 +189,31 @@ function compareLaddersByPlayers(ladderJson1, ladderJson2) {
 }
 compareLaddersByPlayers(ladder230882_1031, ladder230882_1101);
 
-/* Kolik zástupců mají týmy v jednotlivých úrovních ("member -> clan_link" je info o týmu hráče) (info o úrovni ladderu lze získat v JSONu B v "league->league_key->league_id")
- */
+/* Kolik zástupců mají týmy v jednotlivých úrovních ("member -> clan_link" je info o týmu hráče) (info o úrovni ladderu lze získat v JSONu B v "league->league_key->league_id")*/
+function teamsInTier(tierJson) {
+    console.info("Kolik zástupců mají týmy v jednotlivých úrovních (\"member -> clan_link\" je info o týmu hráče) (info o úrovni ladderu lze získat v JSONu B v \"league->league_key->league_id\")");
+    // vypis vsech ladderu
+    let tierLadders = [];
+    tierJson.tier.forEach(division => {
+        tierLadders.push(alasql("SELECT ladder_id AS lad_id FROM ?", [division.division]));
+    });
+    // JOIN pres tabulku, kde prvni bude ladder id a druhej sloupec bude nazev ladder souboru
+    // SELECT tag, id, COUNT(*) FROM tier INNER JOIN ladders ON league_id=league_id GROUP BY tag
+    // pak se kazdy ladder vzpise a udela suma
+    let teamsMembersCount = [];
+    console.log(alasql('SELECT member->(0)->clan_link->clan_tag AS tag, member->(0)->clan_link->id AS id ,COUNT(*) AS members FROM ? GROUP BY member->(0)->clan_link->clan_tag',[ladder230889_1031.team]));
+    alasql('SELECT member->(0)->clan_link->clan_tag AS tag, member->(0)->clan_link->id AS id ,COUNT(*) AS members FROM ? GROUP BY member->(0)->clan_link->clan_tag',[ladder230889_1031.team]).forEach(item => {
+        teamsMembersCount.push(item);
+    });
+    alasql('SELECT member->(0)->clan_link->clan_tag AS tag, member->(0)->clan_link->id AS id ,COUNT(*) AS members FROM ? GROUP BY member->(0)->clan_link->clan_tag',[ladder230898_1031.team]).forEach(item => {
+        teamsMembersCount.push(item);
+    });
+    alasql('SELECT member->(0)->clan_link->clan_tag AS tag, member->(0)->clan_link->id AS id ,COUNT(*) AS members FROM ? GROUP BY member->(0)->clan_link->clan_tag',[ladder230882_1031.team]).forEach(item => {
+        teamsMembersCount.push(item);
+    });
+    // nad polem se vyberou týmy a spočítají
+    console.log(alasql('SELECT tag, SUM(members) FROM ? GROUP BY tag', [teamsMembersCount]));
+}
+teamsInTier(tier5);
+
+
